f7cfc5a788f1cbdac2f45d503f106a8d97f32f53
noahthomlison <noahthomlison@gmail.com>
2021-08-28 20:54:35 +0000

    added functionality to generateRandomString function

--- express_server.js
+++ express_server.js
@@ -2,12 +2,14 @@ const express = require('express')
 const app = express()
 const PORT = 8080
 app.set('view engine', 'ejs')
-
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 }
+
 
 app.get('/urls', (req, res) => {
   const templateVars = { urls: urlDatabase };
@@ -18,7 +20,10 @@ app.get('/urls/new', (req, res) => {
   res.render('urls_new')
 })
 
-
+app.post("/urls", (req, res) => {
+  console.log(req.body);  
+  res.send("Ok");         
+});
 
 app.get("/urls/:shortURL", (req, res) => {
   const longURL = urlDatabase[req.params.shortURL]
@@ -28,10 +33,6 @@ app.get("/urls/:shortURL", (req, res) => {
 
 app.get('/', (req, res) => {
   res.send('Hello')
-})
-
-app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}`)
 })
 
 
@@ -44,3 +45,17 @@ app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
+//function which generates a random 6 letter string
+function generateRandomString() {
+  const alphabet = 'abcdefghijklmnopqrstuvwxyz'
+  let randomString = ''
+  for (let i = 0; i < 6; i++){
+    let index = [Math.floor(Math.random() * 26)]
+    randomString = randomString + alphabet[index]
+  }
+  return (randomString)
+}
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}`)
+})
